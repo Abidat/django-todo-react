4c8dbcf416df8c1a59023d1d9fc7816fd64a8a95
Joda <m.brueckner@abidat.de>
2019-06-20 20:33:00 +0200

    Updated shell scripts to make entire tool actually work with python3.7 and docker.

---
+++ docker/attach_image.sh
@@ -0,0 +1,1 @@
+docker exec -it node_local bash
--- docker/run_image.sh
+++ docker/run_image.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name node_local -p 3000:3000 -v "$PWD/..":/home/node node_local_img /bin/bash
+docker run -it --rm --name node_local -p 3000:3000 -p 8000:8000 -v "$PWD/..":/home/node node_local_img /bin/bash
